6680f71d72437c379566f9f05c9ff7933f6d425d
si2912 <hosi92590@gmail.com>
2021-07-31 06:40:38 +0700

    update content

--- components/Tutorial.vue
+++ components/Tutorial.vue
@@ -1,7 +1,7 @@
 <template>
   <div class="relative flex items-top justify-center min-h-screen bg-gray-100 sm:items-center sm:pt-0">
    <div class="navbar">
-     Hello World
+     Todolist
    </div>
   </div>
 </template>
